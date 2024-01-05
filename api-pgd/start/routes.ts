5b233087617c2125fbfcc28305b214e20952a83e
janio1504 <jcesar_info@yahoo.com.br>
2024-01-05 10:17:00 -0300

    homologacao plano entrega

--- api-pgd/start/routes.ts
+++ api-pgd/start/routes.ts
@@ -65,7 +65,7 @@ Route.group(() => {
 
     Route.post('/create-homologacao-plano-entrega', 'HomologacoesController.createHomologacaoPlanoEntrega')
     Route.get('/get-pe-para-homologar', 'HomologacoesController.getPlanosDeEntrgeParaHomologar')
-    Route.get('/get-pe-homologado-unidade', 'HomologacoesController.getPlanoEntregaUnidadeHomologado')
+    Route.get('/get-pe-homologado-unidade/:id', 'HomologacoesController.getPlanoEntregaUnidadeHomologado')
     Route.post('/homologar-plano-entrega', 'HomologacoesController.homologarPlanoEntrega')
     
 
